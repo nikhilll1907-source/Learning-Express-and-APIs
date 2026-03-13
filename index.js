395318958bc439ae8224de1f9d4f165d8b5cdbf9
Nikhil <nikhilll1907@gmail.com>
2026-03-13 16:34:25 +0530

    first upload

---
+++ index.js
@@ -0,0 +1,39 @@
+const express=require('express');
+const cors = require('cors');
+const app=express();
+app.use(cors())
+
+
+app.use(express.urlencoded({extended:true }))
+app.use(express.json())
+app.get('/',(req,res) => {
+  res.send('jhskjaf') 
+})
+app.post('/sum',(req,res) => {
+    const {a,b}=req.body
+    const c=Number(a)+Number(b)
+    res.json({c:c})
+})
+
+app.post('/sub',(req,res) => {
+   const {a,b}=req.body
+   const c=Number(a)-Number(b)
+   res.json({c})
+})
+app.post('/mul',(req,res) => {
+   const {a,b}=req.body
+   const c=Number(a)*Number(b)
+   res.json({c})
+})
+app.post('/div',(req,res) => {
+   const {a,b}=req.body
+   const c=Number(a)/Number(b)
+   res.json({c})
+})
+console.log(5/2)
+
+const PORT=3000
+app.listen(PORT,() => {
+  console.log(`app is working on http://localhost:${PORT}`);
+})
+
